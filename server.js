392d431d792daee3cc5f607a4c8f0fd367368699
AllenZZP <131452abc@gmail.com>
2022-02-26 13:49:46 -0800

    fix4

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'https://catstrat-lhl.netlify.app/');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Credentials', false);
 
   next();
 });
