3a9965f37d6f2ccf562bfc61d0701c5c1a5e116e
AllenZZP <131452abc@gmail.com>
2022-02-26 03:13:23 -0800

    try2

--- server.js
+++ server.js
@@ -34,6 +34,12 @@ app.use(cors({
   credentials: true
 }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", DEV_URL);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(morgan(ENVIRONMENT));
 
 // allows api to parse json // both .json works but you need to use one
